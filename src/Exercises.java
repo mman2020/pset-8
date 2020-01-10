a917f5cde38c295f6ab0bb361d32980231e8b444
Ryan Wilson <rwilson@ucvts.org>
2020-01-10 13:26:26 -0500

    initial commit

---
+++ src/Exercises.java
@@ -0,0 +1,10 @@
+public class Exercises {
+
+	public boolean commonEnd(int[] a, int[] b) {
+		if (a == null || a.length == 0 || b == null || b.length == 0) {
+			return false;
+		}
+		
+		// write your code here
+	}
+}
